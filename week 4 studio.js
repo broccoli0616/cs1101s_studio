6091bd0839fd78891813b2c1794e3e158abf82f0
Source Academy User <No public email provided>
2024-09-03 09:49:03 +0800

    Changes made from Source Academy

--- week 4 studio.js
+++ week 4 studio.js
@@ -1,1 +1,13 @@
-i cannot finish them aaaa 
+
+
+
+function pascal(a, b){
+    return a <= 1
+    ? 1
+    :a < b 
+    ? 1
+    :b === 0
+    ? 1 
+    : pascal(a - 1, b - 1) + pascal(a - 1, b + 1);
+}
+pascal(4, 3);
