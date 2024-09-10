8dff4218e3f6cecaad5f075aaeb04a4c8c31683e
Source Academy User <No public email provided>
2024-09-10 15:08:56 +0800

    Changes made from Source Academy

--- week 5 studio.js
+++ week 5 studio.js
@@ -1,16 +1,23 @@
-function every_second(items){
+/*function every_second(items){
      function helper(item, n){
-         return n === length(items)
+         return n === length(items) // n > length(items) - 1, if n > length(items), will return consequent
                 ? null
                 : pair(list_ref(items, n), helper(items, n + 2));
      }
     
-    return helper(items, 2);
+    return helper(items, 1);
 }
 
 every_second(list(1, 2, 3, 4, 5, 7, 8, 10)); 
 
-const lst = list(7, list(6, 5, 4), 3, list(2, 1));
+alternative way 
+function every_second(items){
+    return (is_null(items) || is_null(tail(items)))
+        ? null 
+        : pair(head(tail(items)), every_second(tail(tail(items)));
+} */
+
+/*const lst = list(7, list(6, 5, 4), 3, list(2, 1));
 
 head(tail(head(tail(tail(tail(lst)))))); 
 
@@ -31,26 +38,43 @@ head(head(head(tail(tail(lst)))));
 
 
 
+draw_data(list(list(7), list(6, 5, 4), list(3, 2), 1));
+//display_list
+//display */
 
 
+function every_even(items){
+     function helper(item, n){
+         return n > length(items) - 1
+                ? null
+                : pair(list_ref(items, n), helper(items, n + 2));
+     }
+    
+    return helper(items, 0);
+}
 
+function every_odd(items){
+     function helper(item, n){
+         return n > length(items) - 1// n > length(items) - 1, if n > length(items), will return consequent
+                ? null
+                : pair(list_ref(items, n), helper(items, n + 2));
+     }
+    
+    return helper(items, 1);
+}
 
+function sum_value(items){
+    return is_null(items)
+           ? 0
+           : head(items) + sum_value(tail(items));
+}
 
+function sum(items){
+    return list(sum_value(every_even(items)), sum_value(every_odd(items)));
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sum(list(2, 3, 4, 5, 1));
 
 
 
