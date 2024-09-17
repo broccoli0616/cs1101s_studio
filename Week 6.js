7995065ddccc953552736fd45892f74e06bd75c8
Source Academy User <No public email provided>
2024-09-17 09:21:20 +0800

    Changes made from Source Academy

--- Week 6.js
+++ Week 6.js
@@ -1,4 +1,4 @@
-/*function my_map(f, xs){
+function my_map(f, xs){
     return accumulate((x, ys) => pair(f(x), ys), null, xs);
 }
 
@@ -6,7 +6,7 @@ my_map(x => x + 1, list(1, 2, 3));
 
 function remove_duplicates(xs){
     return accumulate(((x, ys) => filter(element !== x ), ys), null, xs);
-} */
+} 
 
 function makeup_amount(x, coins) {
 if (x === 0) {
@@ -15,16 +15,18 @@ return list(null);
 return null;
 } else {
 // Combinations that do not use the head coin.
-const combi_A = makeup_amount(x, tail(coins));
+const combi_A = makeup_amount(x, tail(coins)); // list of list 
 // Combinations that do not use the head coin
 // for the remaining amount.
-const combi_B = makeup_amount(x - head(coins), tail(coins));
+const combi_B = makeup_amount(x - head(coins), tail(coins)); // list of list
 // Combinations that use the head coin.
 const combi_C = map(tree => pair((head(coins)), tree), combi_B);
 return append(combi_A, combi_C);
 }
 }
-// wishful thinking, assume makeup_up amount will return the tree (list of different combi)
+// wishful thinking, assume makeup_up amount will 
+//return the tree (list of different combi)
+
 makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50));
 // Result: list(list(20, 1, 1), list(10, 5, 1, 5, 1), list(1, 20, 1),
 // list(1, 20, 1), list(1, 10, 5, 5, 1),
